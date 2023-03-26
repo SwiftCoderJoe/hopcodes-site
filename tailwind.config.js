dd834f6d75bf32831204e74098c5449e8af5f449
Joe Cardenas <coderjoe05@gmail.com>
2023-03-26 17:00:46 -0400

    rebuild existing UI in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx,vue,html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx,vue,html}'],
     theme: {
+        colors: {
+            sky: colors.sky,
+            slate: colors.slate
+        },
         extend: {},
     },
     plugins: [],
