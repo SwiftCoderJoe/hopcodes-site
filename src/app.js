68358e0ea314cce34865924f4ffaf13af6f365f7
SKlatell24 <114085827+SKlatell24@users.noreply.github.com>
2023-05-06 12:17:05 -0400

    Dark/Light Mode
    Start of Light/Dark Mode (temporary icons don't worry -- I just had to put something in). Haven't figured out how to get the .display-none 2 work yet.

---
+++ src/app.js
@@ -0,0 +1,51 @@
+//https://www.youtube.com/watch?v=oMOe_32M6ss
+
+//ICONS
+const lightThemeIcon = document.querySelector(".lightTheme");
+const darkThemeIcon = document.querySelector(".darkTheme");
+
+//DarkorLightTheme?
+// -- User option:
+const userTheme = localStorage.getItem("theme");
+
+//Stored in Computer:
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+//Icon Toggle
+const iconToggle = () => {
+    lightThemeIcon.classList.toggle.hidden;
+    darkThemeIcon.classList.toggle.hidden;
+};
+
+//Initial Theme (based on system preference):
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+        darkThemeIcon.classList.add("display.none");
+        return;
+    }
+    lightThemeIcon.classList.add("display.none");
+};
+
+//Choose theme Manually
+const themeChoose = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+};
+
+lightThemeIcon.addEventListener("click", () => {
+    themeChoose();
+});
+
+darkThemeIcon.addEventListener("click", () => {
+    themeChoose();
+});
+
+themeCheck();
