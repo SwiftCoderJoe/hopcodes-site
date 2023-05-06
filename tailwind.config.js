1a3a0292f646fc33587a594cff8ef5fb8f88e510
Joe Cardenas <coderjoe05@gmail.com>
2023-05-06 11:42:53 -0400

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-    darkMode: 'class',
     theme: {
         colors: {
             sky: colors.sky,
